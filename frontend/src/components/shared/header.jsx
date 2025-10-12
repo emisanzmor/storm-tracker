1ca0f59583c7525f9d5a3e54bd0008c6bf1db3e7
emisanzmor <emisn1501@gmail.com>
2025-10-12 14:13:48 -0600

    feat(frontend): add Home and About pages, move Dashboard to pages, update Header and Footer

--- frontend/src/components/shared/header.jsx
+++ frontend/src/components/shared/header.jsx
@@ -1,25 +1,36 @@
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
     <header className="fixed top-0 left-0 w-full z-50 flex flex-row backdrop-blur-md bg-black/30">
       <div className="flex flex-col p-4 w-auto">
-        <a href="/" className="pl-4 text-xl text-green-300 whitespace-nowrap">
+        <Link to="/" className="pl-4 text-xl text-green-400 whitespace-nowrap">
           Storm Tracker
-        </a>
+        </Link>
       </div>
       <nav className="w-full p-4 flex justify-end">
         <ul className="flex gap-8 cursor-pointer">
+          {/* DASHBOARD LINK FOR TESTING PURPOSES ONLY */}
           <li>
-            <a
-              href="/about"
+            <Link
+              to="/dashboard"
+              className="text-lg text-white hover:text-green-200"
+            >
+              DashboardDEV
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
               className="text-lg text-white hover:text-green-200"
             >
               About
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="/" className="text-lg text-white hover:text-green-200">
+            <Link to="/" className="text-lg text-white hover:text-green-200">
               Home
-            </a>
+            </Link>
           </li>
         </ul>
       </nav>
